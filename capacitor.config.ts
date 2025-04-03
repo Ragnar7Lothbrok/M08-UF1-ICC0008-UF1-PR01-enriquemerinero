61869e06cfea2da568e1c6d3a57a3e92fb5f3d1d
Ragnar7Lothbrok <kiqqe.7@gmail.com>
2025-04-03 16:13:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ICC0008-UF1-PR01-enriquemerinero',
+  webDir: 'www'
+};
+
+export default config;
