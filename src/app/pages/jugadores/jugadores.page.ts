090f078fb0a80f47c305664d056c7c4c6522e65c
Ragnar7Lothbrok <kiqqe.7@gmail.com>
2025-04-09 16:33:00 +0200

    Nivel 4 completo

--- src/app/pages/jugadores/jugadores.page.ts
+++ src/app/pages/jugadores/jugadores.page.ts
@@ -3,6 +3,8 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { JugadoresService } from '../../services/jugadores.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-jugadores',
@@ -26,11 +28,25 @@ export class JugadoresPage implements OnInit {
   }
 
   abrirCamara(jugador: any) {
-    console.log('📷 Cámara para:', jugador.first_name);
+    Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Prompt
+    }).then(image => {
+      const base64 = `data:image/jpeg;base64,${image.base64String}`;
+      jugador.imagen = base64; // ✅ le añadimos temporalmente la imagen
+  
+      this.verDetalle(jugador); // lo pasamos al detalle
+    });
   }
 
-  compartirJugador(jugador: any) {
-    console.log('📤 Compartir jugador:', jugador.first_name);
+  async compartirJugador(jugador: any) {
+    await Share.share({
+      title: 'Jugador de la NBA',
+      text: `¡Mira este jugador! ${jugador.first_name} ${jugador.last_name}`,
+      dialogTitle: 'Compartir jugador'
+    });
   }
 
   verDetalle(jugador: any) {
